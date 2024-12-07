e8bcedb07c185f4d1959089e4d7635f8d8894ced
franeum <elettrochuck@gmail.com>
2024-12-07 19:33:46 +0100

    update

---
+++ test_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import yaml
+
+PATH = './tree.yaml'
+
+if __name__ == '__main__':
+    with open(PATH, 'r') as f:
+        data = yaml.load(f, Loader=yaml.CLoader)
+
+    print(data.keys())
